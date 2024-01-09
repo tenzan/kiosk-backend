12bead2b738c5b0b48e7403674bd593446c8804e
Askar <askar.karasaev@gmail.com>
2024-01-09 10:46:56 +0900

    Adding backend URL as origin

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,7 +7,7 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:3000'], // Add your frontend URL here
+      origin: ['http://localhost:1337', 'http://localhost:3000', process.env.BACKEND_URL || 'defaultUrl'],
       // You can add other CORS configuration options here, such as methods, headers, etc.
     },
   },
